1fc78deff5d0e3febb5f4d01b4f91d33b9bb5e47
Neenus Gabriel <neenus@live.ca>
2018-09-01 17:44:16 +0000

    fixed add_person.js file to insert into famous_people table from cli

--- add_person.js
+++ add_person.js
@@ -1,17 +1,22 @@
 const settings = require("./settings.json");
-let query = process.argv.slice(2)[0];
+let query = process.argv.slice(2);
 
 var knex = require('knex')({
   client: 'pg',
   connection: settings
 });
 
-knex('famous_people')
-  .insert('')
-  .asCallback((err, rows) => {
-  if (err) {
-    console.error("There is an error", err.stack);
-    return false;
-  }
-  console.log(rows);
-});
+if (query.length != 3) {
+  console.error('Please enter firstname, lastname and birthdat (yyyy-mm-dd)');
+} else (
+  knex('famous_people')
+    .insert({
+      first_name: query[0],
+      last_name: query[1],
+      birthdate: query[2]
+    })
+    .then((result) => {
+      console.log(result);
+      knex.destroy();
+    })
+);
